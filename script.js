dfeb030dc30bbe084a8ecee2dd7feac68338e6fa
Huy <trannhathuy1305@gmail.com>
2023-03-28 18:27:54 +0700

    Done weather app

--- script.js
+++ script.js
@@ -15,6 +15,38 @@ let weather = {
   const { icon, description } = data.weather[0];
   const { temp, humidity } = data.main;
   const { speed } = data.wind;
-  console.log(name, icon, description, temp, humidity, speed);
+  document.querySelector('.city').innerText = 'Weather in ' + name;
+  document.querySelector('.icon').src =
+   'https://openweathermap.org/img/wn/' + icon + '@2x.png';
+  document.querySelector('.description').innerText = description;
+  document.querySelector('.temp').innerText = Math.floor(temp) + '°C';
+  document.querySelector('.humidity').innerText = 'Humidity: ' + humidity + '%';
+  document.querySelector('.wind').innerText = 'Wind speed: ' + speed + ' km/h';
+
+  //   Hide results while loading
+  document.querySelector('.weather').classList.remove('loading');
+
+  //   Background image from search
+  document.body.style.background =
+   "url('https://source.unsplash.com/1600x900/?" + name + "')";
+ },
+
+ //  Get value from search bar
+ search: function () {
+  this.fetchWeather(document.querySelector('.search-bar').value);
  },
 };
+
+// Search button listener
+document.querySelector('.search button').addEventListener('click', function () {
+ weather.search();
+});
+
+// Enter key press listener
+document
+ .querySelector('.search-bar')
+ .addEventListener('keyup', function (event) {
+  if (event.key == 'Enter') {
+   weather.search();
+  }
+ });
